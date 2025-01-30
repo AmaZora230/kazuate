5ceb8a98bf77f13cba59f46a75071a6b616e8900
unknown <amazora230@gmail.com>
2025-01-30 10:24:03 +0900

    レイアウト

--- app/src/main/java/com/example/kazuate/MainActivity.java
+++ app/src/main/java/com/example/kazuate/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.kazuate;
 
+import static android.app.ProgressDialog.show;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +14,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity  {
+
+    //準備
+    private TextView result;
+    private TextView rs1;
+    private TextView rs2;
+    private TextView rs3;
+    private TextView rs4;
+    private TextView rs0;
+    private TextView tv;
+    private EditText edit;
+    private Button sent;
+    private Button reset;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +42,37 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
 
+        //起動と同時
+
+
+
+    }
+
+    //idを取得して準備
+    private void getId() {
         TextView result = findViewById(R.id.result);
         TextView tv = findViewById(R.id.tv);
-        Button sent = findViewById(R.id.sent);
         EditText edit = findViewById(R.id.edit);
         TextView rs0 =findViewById(R.id.rs0);
         TextView rs1 = findViewById(R.id.rs1);
         TextView rs2 = findViewById(R.id.rs2);
         TextView rs3 = findViewById(R.id.rs3);
         TextView rs4 = findViewById(R.id.rs4);
+        Button sent = findViewById(R.id.sent);
         Button reset = findViewById(R.id.reset);
-
-
-
-
-
     }
+
+    //そのあと
+
+
+    //数当てゲームを始めるかどうかの問
+
+
+
+
 
 
 }
