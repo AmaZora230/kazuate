468f438ed4fa3262b9071f2a6776fad0d04399b3
unknown <amazora230@gmail.com>
2025-03-19 11:35:15 +0900

    ランダム変数の更新できていないところの修正

--- app/src/main/java/com/example/kazuate/MainActivity.java
+++ app/src/main/java/com/example/kazuate/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kazuate;
 import static android.app.ProgressDialog.show;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,17 +17,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     //準備
     private TextView result;
-    private TextView rs1;
-    private TextView rs2;
-    private TextView rs3;
-    private TextView rs4;
-    private TextView rs0;
     private TextView tv;
     private EditText edit;
     private Button sent;
@@ -51,74 +46,89 @@ public class MainActivity extends AppCompatActivity {
         });
 
         //起動と同時
-        TextView result = findViewById(R.id.result);
-        TextView tv = findViewById(R.id.tv);
-        EditText edit = findViewById(R.id.edit);
-        Button sent = findViewById(R.id.sent);
-        Button reset = findViewById(R.id.reset);
+        getId();
 
         //数当てゲームを始めるかどうかの問 →　レイアウトで初期設定したため不要
         //初期ランダムの数字を作成
-        int randomNum = random.nextInt(31);
+
+        reGame();
+        Game();
 
         //iの初期設定は不要。（最初の値は0の為）
 
-        //ボタンの設定
-        sent.setOnClickListener( new View.OnClickListener() {
+
+    }
+
+    public void getId(){
+        result = findViewById(R.id.result);
+        tv = findViewById(R.id.tv);
+        edit = findViewById(R.id.edit);
+        sent = findViewById(R.id.sent);
+        reset = findViewById(R.id.reset);
+    }
+
+    public void reGame() {
+
+        randomNum = random.nextInt(31);
+        result.setText(R.string.ResultStart);
+        tv.setText(R.string.TvStart);
+        sent.setEnabled(true);
+        i = 0;
+
+    }
+
+    public void Game(){
+        sent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //空欄だったらトーストで知らせる
                 String inputText = edit.getText().toString();
-                if (inputText.isEmpty()){
-                    Toast.makeText(MainActivity.this,R.string.editText,Toast.LENGTH_LONG).show();
+                if (inputText.isEmpty()) {
+                    Toast.makeText(MainActivity.this, R.string.editText, Toast.LENGTH_LONG).show();
                     return;
                 }
 
                 //空欄じゃなければ実行する
-                 try {
+                try {
                     //入力した数字を受け取る
                     int ans = Integer.parseInt(edit.getText().toString());
 
                     i++; //入力回数
                     edit.setText("");
 
-                    if ( ans == randomNum){
+                    if (ans == randomNum) {
                         result.setText(R.string.correct);
                         tv.setText(R.string.reset);
                         sent.setEnabled(false);
 
-                    } else if ( ans > randomNum ) {
+                    } else if (ans > randomNum) {
                         result.setText(R.string.big);
 
-                    } else if ( ans < randomNum ){
+                    } else if (ans < randomNum) {
                         result.setText(R.string.small);
                     }
-                    if ( i >= 5 ) {
-                        result.setText(getResources().getString(R.string.incorrect,randomNum));
+                    if (i >= 5) {
+                        result.setText(getResources().getString(R.string.incorrect, randomNum));
                         tv.setText(R.string.reset);
                         sent.setEnabled(false);
                     }
                 } catch (NumberFormatException e) {
-                     Toast.makeText(MainActivity.this, R.string.editText, Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, R.string.editText, Toast.LENGTH_LONG).show();
                 }
-
-
-           }
+            }
         });
 
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int randomNum = random.nextInt(31);
-                result.setText(R.string.ResultStart);
-                tv.setText(R.string.TvStart);
-                sent.setEnabled(true);
-                i = 0;
+                reGame();
             }
         });
 
     }
-}
 
+
+
+}
 
 
